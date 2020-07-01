84f4ead3af13268ca646695d2dda03676649a23a
zhaoyue <lx20151020@gmail.com>
2020-07-01 13:58:40 -0700

    hide hideNavigationBar, let it full screen.

--- app/src/main/java/com/example/restaurantlist/MainActivity.java
+++ app/src/main/java/com/example/restaurantlist/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.restaurantlist;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //hide hideNavigationBar, let it full screen.
+        hideNavigationBar();
+
+
+
+
+
+
     }
+
+
+
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private void hideNavigationBar() {
+        //Code found at [https://www.youtube.com/watch?v=cMVbpbaDwTo]
+        this.getWindow().getDecorView()
+                .setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_FULLSCREEN |
+                                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+                                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                );
+    }
+
 }

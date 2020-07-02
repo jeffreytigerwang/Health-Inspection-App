fafed0647d2c396cc8484a9c65ccc45bbd20a6ea
zhaoyue <lx20151020@gmail.com>
2020-07-02 00:53:59 -0700

    set up the Menu activity
    add the list btn

--- app/src/main/java/com/example/restaurantlist/MenuActivity.java
+++ app/src/main/java/com/example/restaurantlist/MenuActivity.java
@@ -2,7 +2,10 @@ package com.example.restaurantlist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -11,5 +14,23 @@ public class MenuActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        // create a btn that user can intend to ListActivity for the list of restaurant.
+        final LinearLayout linearLayout;
+        linearLayout=findViewById(R.id.menu);
+        linearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                linearLayout.setBackground(getDrawable(R.drawable.btn_list_item));
+                Intent intent= new Intent(MenuActivity.this,ListActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
+
     }
 }
--- app/src/main/java/com/example/restaurantlist/welcomeActivity.java
+++ app/src/main/java/com/example/restaurantlist/welcomeActivity.java
@@ -30,10 +30,6 @@ public class welcomeActivity extends AppCompatActivity {
 
 
 
-
-
-
-
     }
 
 
